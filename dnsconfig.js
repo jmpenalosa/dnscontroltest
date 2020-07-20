35fd8011b89217468fb1a3031d50611dca223b4a
Josh Penalosa <49530001+jmpenalosa@users.noreply.github.com>
2020-07-20 10:25:46 -0400

    Add files via upload

---
+++ dnsconfig.js
@@ -0,0 +1,11 @@
+// Providers:
+
+var REG_NONE = NewRegistrar('none', 'NONE');
+var GCLOUD = NewDnsProvider('gcloud', 'GCLOUD');
+
+// Domains:
+
+D('dreamingleo.com', REG_NONE, DnsProvider(GCLOUD),
+    A('@', '104.198.139.177'),
+    A('www', '104.198.139.177')
+);
