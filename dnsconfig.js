e00f58545cb071d4c2f2dd6cc08d7ab1908ca839
jmpenalosa <josuahmari.penalosa@gmail.com>
2020-07-27 22:43:01 +0000

    DNS REVERT

--- dnsconfig.js
+++ dnsconfig.js
@@ -3,12 +3,12 @@
 var REG_NONE = NewRegistrar('none', 'NONE');
 var GCLOUD = NewDnsProvider('gcloud', 'GCLOUD');
 
-var R53 = NewDnsProvider('r53_main', 'ROUTE53');
+// var R53 = NewDnsProvider('r53_main', 'ROUTE53');
 
 
 // Domains:
 
-D('dreamingleo.com', REG_NONE, DnsProvider(GCLOUD), DnsProvider(R53),
+D('dreamingleo.com', REG_NONE, DnsProvider(GCLOUD),
     A('@', '104.198.139.177'),
     A('www', '104.198.139.177'),
     A('awstest', '34.68.48.209')
