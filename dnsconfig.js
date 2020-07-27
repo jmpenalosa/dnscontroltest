dffb112967bd8ac3157d0e2ce2a127b125814ccc
jmpenalosa <josuahmari.penalosa@gmail.com>
2020-07-27 22:27:09 +0000

    Route 53 test 4

--- dnsconfig.js
+++ dnsconfig.js
@@ -1,15 +1,14 @@
 // Providers:
 
 var REG_NONE = NewRegistrar('none', 'NONE');
-
-
 var GCLOUD = NewDnsProvider('gcloud', 'GCLOUD');
+
 var R53 = NewDnsProvider('r53_main', 'ROUTE53');
 
 
 // Domains:
 
-D('dreamingleo.com', REG_NONE, DnsProvider('GCLOUD'), DnsProvider('R53'),
+D('dreamingleo.com', REG, DnsProvider('GCLOUD'), DnsProvider('R53',0),
     A('@', '104.198.139.177'),
     A('www', '104.198.139.177'),
     A('awstest', '34.68.48.209')
