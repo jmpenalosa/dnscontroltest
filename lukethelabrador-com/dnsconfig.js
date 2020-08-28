4785a7a29990e2b15597144d4e66994d76ecdf8a
Josh <josuahmari.penalosa@gmail.com>
2020-08-28 13:32:56 -0700

    secret manager

--- lukethelabrador-com/dnsconfig.js
+++ lukethelabrador-com/dnsconfig.js
@@ -10,7 +10,7 @@ var GCLOUD = NewDnsProvider('gcloud', 'GCLOUD');
 // Domains:
 
 D('blindpuppet.com', REG_NONE, DnsProvider(GCLOUD),
-    A('@', '79.77.77.77'),
+    A('@', '79.79.77.77'),
     A('www', '22.22.22.22'),
     A('band', '55.55.55.55')
 );
