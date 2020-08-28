b1f4112f49e48cbae3dd09eea7973a2c2be8d348
Josh <josuahmari.penalosa@gmail.com>
2020-08-28 13:20:01 -0700

    lukethelabrador

---
+++ lukethelabrador-com/dnsconfig.js
@@ -0,0 +1,18 @@
+// Providers:
+
+var REG_NONE = NewRegistrar('none', 'NONE');
+var GCLOUD = NewDnsProvider('gcloud', 'GCLOUD');
+
+// var REG_NONE = NewRegistrar('none', 'NONE');
+// var R53 = NewDnsProvider('r53_main', 'ROUTE53');
+
+
+// Domains:
+
+D('blindpuppet.com', REG_NONE, DnsProvider(GCLOUD),
+    A('@', '77.77.77.77'),
+    A('www', '22.22.22.22'),
+    A('band', '55.55.55.55')
+);
+
+
